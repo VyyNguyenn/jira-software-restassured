1dabc591f543247501ec80be211cd5e5be8960f4
vynguyen <vytn101@gmail.com>
2023-07-01 00:47:01 +0700

    Refactor TC project

--- src/main/java/utilities/RequestCapabilities.java
+++ src/main/java/utilities/RequestCapabilities.java
@@ -4,7 +4,7 @@ import io.restassured.http.Header;
 import java.util.Base64;
 
 public interface RequestCapabilities {
-    String email = System.getenv("email");
+    String email = "vytn101@gmail.com";
     String token = "ATATT3xFfGF0jb-u9Wd73mm43ql1_ktzYgz71WkgdJhd955cY51Wlgtp-8nr4gCc1ajYV-jW3OimNv2b51J619eVKckIpNXOSDTovuQe9VTe_39lLOvkzuP32uxQAdarbPTaj7JvgUixaKZ32e-57Kfi5LC9yS_YtQm-aREPFq1iZfZFwWX9pgM=A95CDC7C";
     String cred = email.concat(":").concat(token);
     String encodedCred = Base64.getEncoder().encodeToString(cred.getBytes());
