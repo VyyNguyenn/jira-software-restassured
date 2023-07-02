b8698b218bac5427cb13b57e276136a2369fb6c3
vynguyen <vytn101@gmail.com>
2023-07-02 16:20:37 +0700

    Added Allure step and updated pom

--- src/test/java/com/jira/serviceObjects/IssueService.java
+++ src/test/java/com/jira/serviceObjects/IssueService.java
@@ -2,6 +2,7 @@ package com.jira.serviceObjects;
 
 import com.jira.openAPI.issue.DeleteJiraIssue;
 import com.jira.openAPI.issue.PostCreateJiraIssue;
+import io.qameta.allure.Step;
 import io.restassured.http.Header;
 import io.restassured.response.Response;
 
@@ -10,17 +11,20 @@ import static utilities.RequestCapabilities.authorizationHeader;
 
 public class IssueService {
 
+    @Step("Create Jira issue")
     public static Response createIssue(String issueTypeID, String summary, Header...header){
         PostCreateJiraIssue createJiraIssue = new PostCreateJiraIssue();
         Response response = createJiraIssue.createIssue(PROJECT_ID, issueTypeID, summary, header);
         return response;
     };
 
+    @Step("Get issue key")
     public static String getIssueKey(Response response){
         String issueKey = response.then().extract().path("id");
         return issueKey;
     };
 
+    @Step("Delete issue")
     public static Response deleteIssue(String issueKey){
         DeleteJiraIssue deleteJiraIssue = new DeleteJiraIssue();
         Response response = deleteJiraIssue.deleteJiraIssue(issueKey, authorizationHeader);
--- src/test/java/com/jira/serviceObjects/ProjectService.java
+++ src/test/java/com/jira/serviceObjects/ProjectService.java
@@ -1,6 +1,7 @@
 package com.jira.serviceObjects;
 
 import com.jira.openAPI.project.GetJiraProject;
+import io.qameta.allure.Step;
 import io.restassured.http.Header;
 import io.restassured.response.Response;
 import utilities.RequestCapabilities;
@@ -11,18 +12,21 @@ import java.util.Map;
 public class ProjectService {
     static List<Map<String, String>> issueTypes;
 
+    @Step("Get Jira project info")
     public static Response getProject(Header... header){
         GetJiraProject request = new GetJiraProject();
         Response response = request.getProjectInfo(header);
         return response;
     }
 
+    @Step("Get issue list")
     public static void getIssueTypeList(){
         GetJiraProject projectInfo = new GetJiraProject();
         Response response = projectInfo.getProjectInfo(RequestCapabilities.authorizationHeader);
         issueTypes = response.then().extract().path("issueTypes");
     }
 
+    @Step("Get issue ID")
     public static String getIssueTypeID(String issueType){
         getIssueTypeList();
         String issueTypeId = null;
